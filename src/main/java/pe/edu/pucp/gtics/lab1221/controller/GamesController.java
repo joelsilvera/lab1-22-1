f1bca53e4227d8ea7046089a843df6572e4a1a53
Jun23333333 <a20190212@pucp.edu.pe>
2022-09-06 21:24:12 -0500

    listar juego

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
@@ -1,9 +1,32 @@
 package pe.edu.pucp.gtics.lab1221.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.pucp.gtics.lab1221.entity.Games;
+import pe.edu.pucp.gtics.lab1221.repository.GamesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import pe.edu.pucp.gtics.lab1221.repository.PlatformsRepository;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@Controller
+@RequestMapping("/juegos")
 public class GamesController {
 
-    public String listaJuegos (){
-        return "";
+    @Autowired
+    GamesRepository gamesRepository;
+    @Autowired
+    PlatformsRepository platformsRepository;
+
+    @GetMapping("/lista")
+    public String listaJuegos (Model model){
+        List<Games> gamesList = gamesRepository.findAll();
+        model.addAttribute("gamesList",gamesList);
+        return "juegos/lista";
     };
 
     public String editarJuegos(){
