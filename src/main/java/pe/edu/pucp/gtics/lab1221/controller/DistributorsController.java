3bd894bf66bb1f104023e88d9802d95fb2a31a71
Jun23333333 <a20190212@pucp.edu.pe>
2022-09-06 21:07:32 -0500

    pregunta 5 controller

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
@@ -3,6 +3,7 @@ package pe.edu.pucp.gtics.lab1221.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pe.edu.pucp.gtics.lab1221.entity.Distributors;
@@ -40,11 +41,14 @@ public class DistributorsController {
         return "distribuidoras/nuevo";
     };
 
-    public String guardarDistribuidora(){
-        return "";
+    @PostMapping("/guardar")
+    public String guardarDistribuidora(Distributors distributors){
+        distributorsRepository.save(distributors);
+        return "redirect:/distribuidoras/lista";
     };
 
     public String borrarDistribuidora(){
+
         return "";
     };
 
