9a3b855b3826a4a152b23deef39c2fc547a88800
Jun23333333 <a20190212@pucp.edu.pe>
2022-09-06 20:54:36 -0500

    pregunta 2

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
@@ -1,9 +1,25 @@
 package pe.edu.pucp.gtics.lab1221.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.pucp.gtics.lab1221.entity.Distributors;
+import pe.edu.pucp.gtics.lab1221.repository.DistributorsRepository;
+import org.springframework.ui.Model;
+import java.util.List;
+
+@Controller
+@RequestMapping("/distribuidoras")
 public class DistributorsController {
 
-    public String listaDistribuidoras (){
-        return "";
+    @Autowired
+    DistributorsRepository distributorsRepository;
+    @GetMapping("/lista")
+    public String listaDistribuidoras (Model model){
+        List<Distributors> distributorsList = distributorsRepository.findAll();
+        model.addAttribute("distribuidorasList",distributorsList);
+        return "distribuidoras/lista";
     };
 
     public String editarDistribuidoras(){
