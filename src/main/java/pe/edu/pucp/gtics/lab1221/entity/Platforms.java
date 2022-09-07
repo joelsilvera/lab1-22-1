ec28525ccca14d404c31ddacdfeeade4b581175e
Jun23333333 <a20190212@pucp.edu.pe>
2022-09-06 21:18:41 -0500

    pregunta 6

--- src/main/java/pe/edu/pucp/gtics/lab1221/entity/Platforms.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/entity/Platforms.java
@@ -1,6 +1,45 @@
 package pe.edu.pucp.gtics.lab1221.entity;
 
-
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.*;
+@Entity
+@Table(name = "plataformas")
 public class Platforms {
-    /** Completar*/
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idplataforma")
+    private Integer id;
+
+    @Column(name = "nombre", length = 50)
+    private String nombre;
+
+    @Column(name = "descripcion", length = 200)
+    private String descripcion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- src/main/java/pe/edu/pucp/gtics/lab1221/repository/PlatformsRepository.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/repository/PlatformsRepository.java
@@ -1,4 +1,9 @@
 package pe.edu.pucp.gtics.lab1221.repository;
 
-public interface PlatformsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.pucp.gtics.lab1221.entity.Platforms;
+
+@Repository
+public interface PlatformsRepository extends JpaRepository<Platforms, Integer> {
 }
