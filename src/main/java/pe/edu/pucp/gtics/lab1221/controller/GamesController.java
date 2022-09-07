aaf3d7a21f0cf2647632d4d03cc2af5a7f8d81d0
Jun23333333 <a20190212@pucp.edu.pe>
2022-09-06 21:30:53 -0500

    lo resto del juego

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
@@ -3,8 +3,11 @@ package pe.edu.pucp.gtics.lab1221.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pe.edu.pucp.gtics.lab1221.entity.Games;
+import pe.edu.pucp.gtics.lab1221.entity.Platforms;
 import pe.edu.pucp.gtics.lab1221.repository.GamesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import pe.edu.pucp.gtics.lab1221.repository.PlatformsRepository;
@@ -29,12 +32,26 @@ public class GamesController {
         return "juegos/lista";
     };
 
-    public String editarJuegos(){
-        return "";
+    @GetMapping("/editar")
+    public String editarJuegos(@RequestParam("id") int id, Model model) {
+        List<Platforms> platformsList = platformsRepository.findAll();
+        model.addAttribute("platformsList",platformsList);
+
+        Optional<Games> optionalGames = gamesRepository.findById(id);
+        if(optionalGames.isPresent()){
+            Games games = optionalGames.get();
+            model.addAttribute("games", games);
+            return "juegos/editar";
+        }else{
+            return "redirect:/juegos/lista";
+        }
+
     };
 
-    public String guardarJuegos(){
-        return "";
+    @PostMapping("/guardar")
+    public String guardarJuegos(Games games){
+        gamesRepository.save(games);
+        return "redirect:/juegos/lista";
     };
 
 }
